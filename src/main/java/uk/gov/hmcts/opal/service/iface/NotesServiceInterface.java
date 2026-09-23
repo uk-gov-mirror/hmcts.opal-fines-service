hmcts/opal-fines-service

PO-8990 updated add notes endpoint to use open API definition

src/main/java/uk/gov/hmcts/opal/service/iface/NotesServiceInterface.java
@@ -1,11 +1,11 @@
 package uk.gov.hmcts.opal.service.iface;
 
 import uk.gov.hmcts.opal.common.user.authorisation.model.UserState;
-import uk.gov.hmcts.opal.dto.AddNoteRequest;
+import uk.gov.hmcts.opal.generated.model.AddNoteRequestNotes;
 import uk.gov.hmcts.opal.service.AccountNoteContext;
 
 public interface NotesServiceInterface {
 
-    String addNote(AddNoteRequest request, String ifMatch, UserState user, AccountNoteContext target);
+    String addNote(AddNoteRequestNotes request, String ifMatch, UserState user, AccountNoteContext target);
 
 }

uk-gov-mirror/hmcts.opal-fines-service

src/main/java/uk/gov/hmcts/opal/service/iface/NotesServiceInterface.java
@@ -1,10 +1,10 @@
 package uk.gov.hmcts.opal.service.iface;
 
 import uk.gov.hmcts.opal.common.user.authorisation.model.UserState;
-import uk.gov.hmcts.opal.dto.AddNoteRequest;
+import uk.gov.hmcts.opal.generated.model.AddNoteRequestNotes;
 
 public interface NotesServiceInterface {
 
-    String addNote(AddNoteRequest request, String ifMatch, UserState user, Short businessUnitId);
+    String addNote(AddNoteRequestNotes request, String ifMatch, UserState user, Short businessUnitId);
 
 }